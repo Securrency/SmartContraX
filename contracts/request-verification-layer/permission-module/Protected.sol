592dc62ee1ca611eec35f7179af9054ec673dd22
Ihor Yermakov <ihor.yermakov@dev-pro.net>
2018-09-04 15:29:08 +0300

    Permission module. Protected

---
+++ contracts/request-verification-layer/permission-module/Protected.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.4.24;
+
+import "./interfaces/IPermissionModule.sol";
+
+/**
+* @title Protected
+*/
+contract Protected {
+    // Permission module address
+    address pm;
+
+    /**
+    * @notice Verify permission for the method and sender wallet
+    * @param method Requested method
+    * @param sender Transaction sender address
+    */
+    modifier verifyPermission(bytes4 method, address sender) {
+        require(IPermissionModule(pm).allowed(method, sender, address(0)), "Declined by Permission Module.");
+        _;
+    }
+
+    /**
+    * @notice Verify permission for the method, sender and for the token
+    * @param method Requested method
+    * @param sender Transaction sender address
+    * @param token Token address
+    */
+    modifier verifyPermissionForToken(bytes4 method, address sender, address token) {
+        require(IPermissionModule(pm).allowed(method, sender, token), "Declined by Permission Module.");
+        _;
+    }
+
+    /**
+    * @notice Initialize contract with permission module
+    */
+    constructor(address permissionMudule) public {
+        pm = permissionMudule;
+    }
+}
