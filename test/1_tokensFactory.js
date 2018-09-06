7c948097f2aa0c168bf31700acc50b4d19c376ff
Ihor Yermakov <ihor.yermakov@dev-pro.net>
2018-09-06 13:05:50 +0300

    Updated transferFrom test

--- test/1_tokensFactory.js
+++ test/1_tokensFactory.js
@@ -493,9 +493,9 @@ contract('TokensFactory', accounts => {
         it("Should transfer approved tokens", async() => {
             let tx = await SLS20Token.transferFrom(token_owner, token_holder_2, toApprove, {from: token_holder_1});
 
-            assert.equal(tx.logs[0].args.from, token_owner);
-            assert.equal(tx.logs[0].args.to, token_holder_2);
-            assert.equal(tx.logs[0].args.value.toNumber(), toApprove);
+            assert.equal(tx.logs[1].args.from, token_owner);
+            assert.equal(tx.logs[1].args.to, token_holder_2);
+            assert.equal(tx.logs[1].args.value.toNumber(), toApprove);
 
             let balance = await SLS20Token.balanceOf(token_holder_2);
             
