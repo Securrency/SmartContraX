59cef3015812b3cd75885f4c20a6fb6f0f2f0554
Ihor Yermakov <ihor.yermakov@dev-pro.net>
2018-11-13 17:36:22 +0200

    CAT-20. Added functionality for tokens burning

---
+++ contracts/registry-layer/tokens-factory/tokens/CAT-20/CAT20Burnable.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.4.24;
+
+import "./SecuritiesStandardToken.sol";
+
+
+/**
+* @title Extends CAT-20. Allows burn tokens.
+*/
+contract CAT20Burnale is SecuritiesStandardToken {
+    /**
+    * @notice Write info to the log about burned tokens
+    * @param from Address from which tokens were burned
+    * @param value Number of burned tokens
+    */
+    event Burn(address indexed from, uint256 value);
+
+    /**
+    * @notice Write info to the log when issuer was burned tokens
+    * @param from Address from which tokens were burned
+    * @param burnedBy Address of the transaction initiator
+    * @param value Number of burned tokens
+    * @param data Additional data which will be added to the log
+    */
+    event BurnedByIssuer(address indexed from, address indexed burnedBy, uint256 value, bytes32 data);
+
+    /**
+    * @notice Burns a specific amount of tokens for sender
+    * @param value The amount of token to be burned
+    */
+    function burn(uint value) public {
+        _burn(msg.sender, value);
+    }
+
+    /**
+    * @notice Burns a specific amount of tokens for selected address
+    * @notice Protected by permission module
+    * @param from Address from which tokens were burned
+    * @param value The amount of token to be burned
+    * @param data Additional data which will be added to the log
+    */
+    function issuerBurn(address from, uint value, bytes32 data)
+        external
+        verifyPermission(msg.sig, msg.sender)
+    {
+        _burn(from, value);
+
+        emit BurnedByIssuer(from, msg.sender, value, data);
+    }
+
+    /**
+    * @notice Burns a specific amount of tokens.
+    * @param from Address from which tokens were burned
+    * @param value The amount of token to be burned.
+    */
+    function _burn(address from, uint value) internal {
+        require(value <= balances[from]);
+
+        // Stats updates
+        balances[from] = balances[from].sub(value);
+        totalSupply_ = totalSupply_.sub(value);
+
+        // Write info to the log
+        emit Burn(from, value);
+        emit Transfer(from, address(0), value);
+    }
+}
--- contracts/registry-layer/tokens-factory/tokens/CAT-20/CAT20Token.sol
+++ contracts/registry-layer/tokens-factory/tokens/CAT-20/CAT20Token.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
 import "./SecuritiesStandardToken.sol";
 import "./CAT20Mintable.sol";
+import "./CAT20Burnable.sol";
 
 
 /**
  * @title Compliance Aware Token (CAT-20), an extension of an ERC20 token standard
  */
-contract CAT20Token is SecuritiesStandardToken, CAT20Mintable, DetailedERC20 {
+contract CAT20Token is SecuritiesStandardToken, CAT20Mintable, CAT20Burnale, DetailedERC20 {
     constructor(
         string _name,
         string _symbol,
