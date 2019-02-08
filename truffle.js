b6d782d286210242e76a315a692eb6d208dfe5bc
Ihor Yermakov <ihor.yermakov@dev-pro.net>
2019-02-08 15:33:12 +0200

    truffle.js. Updated to the hdwallet provider 1.0.3 requirements

--- truffle.js
+++ truffle.js
@@ -11,25 +11,25 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     kovan: {
-      provider: new HDWalletProvider(privateKey, "https://kovan.infura.io/"),
+      provider: () => new HDWalletProvider(privateKey, "https://kovan.infura.io/"),
       network_id: "42",
       gas: 4500000,
       gasPrice: 5000000000
     },
     ropsten: {
-      provider: new HDWalletProvider(privateKey, "https://ropsten.infura.io/"),
+      provider: () => new HDWalletProvider(privateKey, "https://ropsten.infura.io/"),
       network_id: "3",
       gas: 4500000,
       gasPrice: 5000000000
     },
     mainnet: {
-      provider: new HDWalletProvider(privateKey, "https://mainnet.infura.io/"),
+      provider: () => new HDWalletProvider(privateKey, "https://mainnet.infura.io/"),
       network_id: "1",
       gas: 450000,
       gasPrice: 10000000000
     },
     gochain: {
-      provider: new HDWalletProvider(privateKey, "https://testnet-rpc.gochain.io"),
+      provider: () => new HDWalletProvider(privateKey, "https://testnet-rpc.gochain.io"),
       network_id: "*",
       gas: 4500000,
       gasPrice: 5000000000
