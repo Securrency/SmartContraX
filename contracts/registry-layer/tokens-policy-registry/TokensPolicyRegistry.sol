7080b2ae3c0b3a6fe5e212e7292acec354b9ad3a
Ihor Yermakov <ihor.yermakov@dev-pro.net>
2019-02-15 14:08:26 +0200

    Tokens policy registry. Added sub ids

--- contracts/registry-layer/tokens-policy-registry/TokensPolicyRegistry.sol
+++ contracts/registry-layer/tokens-policy-registry/TokensPolicyRegistry.sol
@@ -17,6 +17,15 @@ contract TokensPolicyRegistry is ITokensPolicyRegistry, Protected, TokensFactory
     // token => action => policy hash
     mapping(address => mapping(bytes32 => bytes32)) policyHash;
 
+    // Declare storage for policies with ids
+    // token => action => id => policy
+    mapping(address => mapping(bytes32 => mapping(bytes32 => bytes))) policiesWithIds;
+
+    // Declare storage for policy hash
+    // token => action => id => policy hash
+    mapping(address => mapping(bytes32 => mapping(bytes32 => bytes32))) policyWithIdHash;
+
+
     // Write info to the log when a policy was set
     event Policy(address indexed token, bytes32 action, bytes policy);
     
@@ -58,12 +67,49 @@ contract TokensPolicyRegistry is ITokensPolicyRegistry, Protected, TokensFactory
     }
 
     /**
+    * @notice Set policy for the token action
+    * @param token Token address
+    * @param action Action
+    * @param id Additional identifier
+    * @param policy Token policy for the specified action
+    */
+    function setPolicyWithId(
+        address token,
+        bytes32 action,
+        bytes32 id,
+        bytes calldata policy
+    )
+        external
+        allowedForTokenWithSubId(msg.sig, msg.sender, token, id)
+        onlyRegisteredToken(token)
+    {
+        require(token != address(0), "Invalid token address");
+        require(action != bytes32(0x00), "Invalid action");
+        require(id != bytes32(0x00), "Invalid identifier");
+
+        policiesWithIds[token][action][id] = policy;
+        policyWithIdHash[token][action][id] = keccak256(policy);
+
+        emit Policy(token, action, policy);
+    }
+
+    /**
     * @notice Returns policy for specified action and token
     * @param token Token address
     * @param action Action
     */
     function getPolicy(address token, bytes32 action) public view returns (bytes memory) {
         return policies[token][action];
+    }
+
+    /**
+    * @notice Returns policy for specified action and token
+    * @param token Token address
+    * @param action Action
+    * @param id Additional id
+    */
+    function getPolicyById(address token, bytes32 action, bytes32 id) public view returns (bytes memory) {
+        return policiesWithIds[token][action][id];
     }
 
     /**
@@ -76,11 +122,31 @@ contract TokensPolicyRegistry is ITokensPolicyRegistry, Protected, TokensFactory
     }
 
     /**
+    * @notice Returns policy hash for specified action and token
+    * @param token Token address
+    * @param action Action
+    * @param id Additional id
+    */
+    function getPolicyHashById(address token, bytes32 action, bytes32 id) public view returns (bytes32) {
+        return policyWithIdHash[token][action][id];
+    }
+
+    /**
     * @notice Returns policy length
     * @param token Token address
     * @param action Action
     */
     function getPolicyLength(address token, bytes32 action) public view returns (uint) {
         return policies[token][action].length;
+    }
+
+    /**
+    * @notice Returns policy length
+    * @param token Token address
+    * @param action Action
+    * @param id Additional id
+    */
+    function getPolicyWithIdLength(address token, bytes32 action, bytes32 id) public view returns (uint) {
+        return policiesWithIds[token][action][id].length;
     }
 }
--- contracts/registry-layer/tokens-policy-registry/interfaces/ITokensPolicyRegistry.sol
+++ contracts/registry-layer/tokens-policy-registry/interfaces/ITokensPolicyRegistry.sol
@@ -14,11 +14,34 @@ contract ITokensPolicyRegistry {
     function setPolicy(address token, bytes32 action, bytes calldata policy) external;
 
     /**
+    * @notice Set policy for the token action
+    * @param token Token address
+    * @param action Action
+    * @param id Additional identifier
+    * @param policy Token policy for the specified action
+    */
+    function setPolicyWithId(
+        address token,
+        bytes32 action,
+        bytes32 id,
+        bytes calldata policy
+    ) 
+        external;
+
+    /**
     * @notice Returns policy for specified action and token
     * @param token Token address
     * @param action Action
     */
     function getPolicy(address token, bytes32 action) public view returns (bytes memory);
+
+    /**
+    * @notice Returns policy for specified action and token
+    * @param token Token address
+    * @param action Action
+    * @param id Additional id
+    */
+    function getPolicyById(address token, bytes32 action, bytes32 id) public view returns (bytes memory);
 
     /**
     * @notice Returns policy hash for specified action and token
@@ -28,9 +51,25 @@ contract ITokensPolicyRegistry {
     function getPolicyHash(address token, bytes32 action) public view returns (bytes32);
 
     /**
+    * @notice Returns policy hash for specified action and token
+    * @param token Token address
+    * @param action Action
+    * @param id Additional id
+    */
+    function getPolicyHashById(address token, bytes32 action, bytes32 id) public view returns (bytes32);
+
+    /**
     * @notice Returns policy length
     * @param token Token address
     * @param action Action
     */
     function getPolicyLength(address token, bytes32 action) public view returns (uint);
+
+    /**
+    * @notice Returns policy length
+    * @param token Token address
+    * @param action Action
+    * @param id Additional id
+    */
+    function getPolicyWithIdLength(address token, bytes32 action, bytes32 id) public view returns (uint);
 }
