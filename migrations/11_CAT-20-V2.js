d50cef30a575973215fd09951f02687121774550
Ihor Yermakov <ihor.yermakov@dev-pro.net>
2019-04-05 12:14:41 +0300

    Migrations. CAT-20 new functions

--- migrations/11_CAT-20-V2.js
+++ migrations/11_CAT-20-V2.js
@@ -1,10 +1,10 @@
 var ComponentsRegistry = artifacts.require("./registry-layer/components-registry/ComponentsRegistry.sol");
-var setupV1 = artifacts.require("./registry-layer/tokens-factory/tokens/CAT-20-V2/token-setup/SetupV1.sol");
+var CAT20Setup = artifacts.require("./registry-layer/tokens-factory/tokens/CAT-20-V2/CAT20Setup.sol");
 var CAT20V2Strategy = artifacts.require("./registry-layer/tokens-factory/deployment-strategies/CAT20V2Strategy.sol");
 
 module.exports = async function(deployer) {
   var ComponentsRegistryDeployed = await ComponentsRegistry.deployed();
 
-  await deployer.deploy(setupV1, {gas: 600000});
-  await deployer.deploy(CAT20V2Strategy, ComponentsRegistryDeployed.address, setupV1.address, {gas: 1200000});
+  await deployer.deploy(CAT20Setup, {gas: 1000000});
+  await deployer.deploy(CAT20V2Strategy, ComponentsRegistryDeployed.address, CAT20Setup.address, {gas: 1300000});
 };
--- migrations/12_CAT-20-functions.js
+++ migrations/12_CAT-20-functions.js
@@ -1,14 +1,22 @@
 // CAT-20-V2 functions
-var ERC20Functions = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/CAT-20-functions/ERC20Functions.sol");
-var CAT20Mint = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/CAT-20-functions/CAT20MintFunction.sol");
-var CAT20TransferWithWL = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/CAT-20-functions/CAT20WLVTransferFunction.sol");
-var CAT20TransferWithRE = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/CAT-20-functions/CAT20REVTransferFunction.sol");
-var CAT20ClawbackWithWL = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/CAT-20-functions/CAT20WLVClawbackFunction.sol");
+var CAT20ERC20 = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/CAT20ERC20.sol");
+var CAT20Mint = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/CAT20Mint.sol");
+var CAT20Burn = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/CAT20Burnable.sol");
+var CAT20Pause = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/CAT20Pause.sol");
+var CAT20Rollback = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/CAT20Rollback.sol");
+var CAT20Clawback = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/CAT20Clawback.sol");
+var CAT20REVerifyTransfer = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/verify-transfer/CAT20REVerifyTransfer.sol");
+var CAT20WLVerifyTransfer = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/verify-transfer/CAT20WLVerifyTransfer.sol");
+var CAT20TransferWithoutVerification = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/verify-transfer/CAT20TransferWithoutVerification.sol");
 
 module.exports = async function(deployer) {
-  await deployer.deploy(ERC20Functions, {gas:700000});
-  await deployer.deploy(CAT20Mint, {gas:300000});
-  await deployer.deploy(CAT20TransferWithWL, {gas:600000});
-  await deployer.deploy(CAT20TransferWithRE, {gas:600000});
-  await deployer.deploy(CAT20ClawbackWithWL, {gas:500000});
+  await deployer.deploy(CAT20ERC20, {gas:1300000});
+  await deployer.deploy(CAT20Mint, {gas:1100000});
+  await deployer.deploy(CAT20Clawback, {gas:1100000}); 
+  await deployer.deploy(CAT20REVerifyTransfer, {gas:1100000});
+  await deployer.deploy(CAT20WLVerifyTransfer, {gas:1100000});
+  await deployer.deploy(CAT20TransferWithoutVerification, {gas:1100000});
+  await deployer.deploy(CAT20Burn, {gas:800000});
+  await deployer.deploy(CAT20Pause, {gas:800000});
+  await deployer.deploy(CAT20Rollback, {gas:1300000});
 };
