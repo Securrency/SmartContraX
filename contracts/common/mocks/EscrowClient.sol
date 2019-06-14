4bcde3fb9780254b87a5724495a18686d0f1f08e
Ihor Yermakov <ihor.yermakov@dev-pro.net>
2019-06-14 15:06:41 +0300

    Escrow client mock. Changed solidity version

--- contracts/common/mocks/EscrowClient.sol
+++ contracts/common/mocks/EscrowClient.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.0;
+pragma solidity >=0.5.0 <0.6.0;
 
 import "../introspection/SupportsInterfaceWithLookup.sol";
 import "../../registry-layer/tokens-factory/interfaces/ICAT20EscrowCanceled.sol";
