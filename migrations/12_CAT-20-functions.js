7c04319b6ecc88f89a5ad02e588f5edacb48446f
Ihor Yermakov <ihor.yermakov@dev-pro.net>
2019-06-14 15:23:16 +0300

    Migrations. CAT-20 documents

--- migrations/12_CAT-20-functions.js
+++ migrations/12_CAT-20-functions.js
@@ -8,6 +8,7 @@ var CAT20Clawback = artifacts.require("./registry-layer/tokens-factory/token/CAT
 var CAT20REVerifyTransfer = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/verify-transfer/CAT20REVerifyTransfer.sol");
 var CAT20WLVerifyTransfer = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/verify-transfer/CAT20WLVerifyTransfer.sol");
 var CAT20TransferWithoutVerification = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/verify-transfer/CAT20TransferWithoutVerification.sol");
+var CAT20Documents = artifacts.require("./registry-layer/tokens-factory/token/CAT-20-V2/functions/CAT20Documents.sol");
 
 module.exports = async function(deployer) {
   await deployer.deploy(CAT20ERC20, {gas:1300000});
@@ -19,4 +20,5 @@ module.exports = async function(deployer) {
   await deployer.deploy(CAT20Burn, {gas:800000});
   await deployer.deploy(CAT20Pause, {gas:800000});
   await deployer.deploy(CAT20Rollback, {gas:1300000});
+  await deployer.deploy(CAT20Documents, {gas:1300000});
 };
